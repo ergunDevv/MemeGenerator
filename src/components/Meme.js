2444187713055a07c3e8d56a23a3c7a0c7e1a51c
ergunDevv <ergncyln2127088@gmail.com>
2022-11-13 19:21:15 +0300

    Final Project with css(tailwind)

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -60,7 +60,7 @@ export default function Meme() {
             </div>
             <div className="meme">
                 <img src={meme.randomImage} className="meme--image" alt="meme-img" />
-                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text top ">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
